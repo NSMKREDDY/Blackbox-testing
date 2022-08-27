2eafcf868098575e24d5072dd26151d71e9698c9
NSMKREDDY <112233314+NSMKREDDY@users.noreply.github.com>
2022-08-27 11:01:03 +0530

    Added Keyword Arguments file
    I Added Keyword Arguments file from Python folder

---
+++ Key_arg.py
@@ -0,0 +1,18 @@
+def add(a,b,c):
+    print("A Value is:",a)
+    print("B Value is:",b)
+    print("C Value is:",c)
+    print('\n',"The Value of a,b and c is:", a+b+c)
+
+'''add(c=30,b=40,a=50)
+
+add (20,30,a=50)
+
+add(a=20,30,c=50)'''
+
+add(a=20,b=30,c=50)
+
+add (c=50,a=20,b=30)
+
+add (80,b=50,c=60)
+
